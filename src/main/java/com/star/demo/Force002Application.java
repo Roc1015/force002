75a7c168f6f3365bbcc10ae9b04ae9ac2fdedaef
WP <1344471553@qq.com>
2020-12-29 15:54:48 +0800

    frist commit

---
+++ src/main/java/com/star/demo/Force002Application.java
@@ -0,0 +1,13 @@
+package com.star.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Force002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Force002Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/demo/Force002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Force002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
